a9d08a520b61da936af4bf33e492174b7086ba0c
QueenieCplusplus <queeniecplusplus@gmail.com>
2020-08-07 09:52:16 +0800

    firstly, to do login button to redirect window

---
+++ githubauthapp/src/App.js
@@ -0,0 +1,23 @@
+// 2020, AM 9:30-10:30 (1 hr)
+
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom'; // 使用此Router將其他元件包覆起來
+import { gql } from 'apollo-boost'
+import AuthUser from './AuthUser' //(等待建立)
+import User from './User' //(等待建立)
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <AuthUser/>
+        <User/>
+      </div>
+    </BrowserRouter>
+  );
+
+}
+
+export default App;
